c51c0f1969a623d2a08d56b8773caa9abf6c4620
Svein Manum <svein.manum@gmail.com>
2023-08-31 13:18:11 +0200

    Delete old backend files

--- db-scheduler-ui-backend/src/main/java/com/github/bekk/dbscheduleruibackend/model/TaskType.java
+++
@@ -1,7 +0,0 @@
-package com.github.bekk.dbscheduleruibackend.model;
-
-public enum TaskType {
-    ONE_TIME,
-    RECURRING,
-    CHAIN
-}
--- db-scheduler-ui/src/main/java/com/github/bekk/dbscheduleruiapi/model/TaskRequestParams.java
+++ db-scheduler-ui/src/main/java/com/github/bekk/dbscheduleruiapi/model/TaskRequestParams.java
@@ -1,19 +1,23 @@
 package com.github.bekk.dbscheduleruiapi.model;
 
 public class TaskRequestParams {
-    
+
     private TaskFilter filter;
     private int pageNumber;
     private int size;
+    private TaskSort sorting;
+    private boolean asc;
 
     public TaskRequestParams() {
     }
 
-    public TaskRequestParams(TaskFilter filter, int pageNumber, int size) {
+    public TaskRequestParams(TaskFilter filter, int pageNumber, int size, TaskSort sorting, boolean asc) {
         System.out.println(size);
         this.filter = filter;
         this.pageNumber = pageNumber;
         this.size = size;
+        this.sorting = sorting;
+        this.asc = asc;
     }
 
     public TaskFilter getFilter() {
@@ -39,10 +43,30 @@ public class TaskRequestParams {
     public void setSize(int size) {
         this.size = size;
     }
+    public TaskSort getSorting() {
+        return sorting;
+    }
+
+    public void setSorting(TaskSort sorting) {
+        this.sorting = sorting;
+    }
+
+    public boolean isAsc() {
+        return asc;
+    }
+
+    public void setAsc(boolean asc) {
+        this.asc = asc;
+    }
 
     public enum TaskFilter {
+        ALL,
         FAILED,
         RUNNING,
         SCHEDULED;
+    }
+    public enum TaskSort {
+        DEFAULT,
+        NAME;
     }
 }
