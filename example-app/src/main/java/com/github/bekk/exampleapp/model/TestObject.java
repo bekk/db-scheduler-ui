617a11a1b317f2ab10191310f1a02cffbdda130c
johannabjoremo <johanna.bjoremo@gmail.com>
2023-09-04 15:09:45 +0200

    ChainTask.java holds data, and starts in loop. (#16)

---
+++ example-app/src/main/java/com/github/bekk/exampleapp/model/TestObject.java
@@ -0,0 +1,41 @@
+package com.github.bekk.exampleapp.model;
+
+import java.io.Serializable;
+
+public class TestObject implements Serializable {
+    private String name;
+    private int id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public TestObject(String initialName, int id, String email) {
+        this.id = id;
+        this.name = initialName;
+        this.email = email;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
--- example-app/src/main/java/com/github/bekk/exampleapp/tasks/TaskDefinitions.java
+++ example-app/src/main/java/com/github/bekk/exampleapp/tasks/TaskDefinitions.java
@@ -3,6 +3,7 @@ package com.github.bekk.exampleapp.tasks;
 import com.github.bekk.exampleapp.model.TaskData;
 import com.github.kagkarlsson.scheduler.task.Task;
 import com.github.kagkarlsson.scheduler.task.TaskInstance;
+import com.github.bekk.exampleapp.model.TestObject;
 
 import java.util.List;
 
@@ -28,8 +29,7 @@ public class TaskDefinitions {
         return List.of(
                 OneTimeTaskExample.getExample().instance("1", new TaskData(1, "test data")),
                 RecurringTaskExample.getExample().instance("2"),
-                ChainTask.chainTaskStepOne().instance("3"),
-                ChainTask.chainTaskStepTwo().instance("4"),
+                ChainTask.chainTaskStepOne().instance("3", new TestObject("Ole Nordman", 3, "ole.nordman@mail.com")),
                 LongRunningTask.runLongRunningTask().instance("5"),
                 FailingTask.runOneTimeFailing().instance("6"),
                 FailingTask.runRecurringFailing().instance("7"),
