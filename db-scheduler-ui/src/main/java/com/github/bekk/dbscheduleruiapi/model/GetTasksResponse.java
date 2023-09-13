d45a7e3357aa48a18bd39e9704629fa2814cc117
Vegard Smines <vegard.smines@bekk.no>
2023-09-13 17:21:11 +0200

    added search in tests

--- db-scheduler-ui/src/main/java/com/github/bekk/dbscheduleruiapi/model/GetTasksResponse.java
+++ db-scheduler-ui/src/main/java/com/github/bekk/dbscheduleruiapi/model/GetTasksResponse.java
@@ -1,41 +1,34 @@
 package com.github.bekk.dbscheduleruiapi.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class GetTasksResponse {
-  private int numberOfTasks;
-  private int numberOfPages;
-  private List<TaskModel> tasks;
+  private final int numberOfTasks;
+  private final int numberOfPages;
+  private final List<TaskModel> tasks;
 
-  public GetTasksResponse(int totalTasks, List<TaskModel> pagedTasks, int pageSize) {
+  @JsonCreator
+  public GetTasksResponse(
+          @JsonProperty("numberOfTasks") int totalTasks,
+          @JsonProperty("tasks") List<TaskModel> pagedTasks,
+          @JsonProperty("pageSize") int pageSize) {
     this.numberOfTasks = totalTasks;
     this.numberOfPages = totalTasks == 0 ? 0 : (int) Math.ceil((double) totalTasks / pageSize);
     this.tasks = pagedTasks;
   }
 
-  public GetTasksResponse() {}
-
   public int getNumberOfTasks() {
     return numberOfTasks;
-  }
-
-  public void setNumberOfTasks(int numberOfTasks) {
-    this.numberOfTasks = numberOfTasks;
   }
 
   public int getNumberOfPages() {
     return numberOfPages;
   }
 
-  public void setNumberOfPages(int numberOfPages) {
-    this.numberOfPages = numberOfPages;
-  }
-
   public List<TaskModel> getTasks() {
     return tasks;
-  }
-
-  public void setTasks(List<TaskModel> tasks) {
-    this.tasks = tasks;
   }
 }
