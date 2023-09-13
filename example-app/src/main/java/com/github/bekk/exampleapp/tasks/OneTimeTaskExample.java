e153ea53aaefab34e387ff90f8e06895aa5be95e
Johanna <johanna.bjoremo@bekk.no>
2023-09-13 11:18:42 +0200

    Removed sleep in OneTimeTaskExample

--- example-app/src/main/java/com/github/bekk/exampleapp/tasks/OneTimeTaskExample.java
+++ example-app/src/main/java/com/github/bekk/exampleapp/tasks/OneTimeTaskExample.java
@@ -5,7 +5,6 @@ import com.github.kagkarlsson.scheduler.task.TaskWithDataDescriptor;
 import com.github.kagkarlsson.scheduler.task.helper.OneTimeTask;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 
-import static utils.Utils.sleep;
 
 public class OneTimeTaskExample {
 
@@ -15,7 +14,6 @@ public class OneTimeTaskExample {
     public static OneTimeTask<TaskData> getExample() {
         return Tasks.oneTime(ONE_TIME_TASK).
                 execute((inst,ctx) -> {
-                    sleep(300);
                     System.out.println("Executed onetime task: " + inst.getTaskName());
                     System.out.println("With data id: " + inst.getData().getId() + " data: " + inst.getData().getData());
                 });
